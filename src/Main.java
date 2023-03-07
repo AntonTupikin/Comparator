863af75fb9657c824c4f914a7cef7668b0771af4
AntonTupikin <antontupikin@icloud.com>
2023-03-07 09:01:47 +0500

    1.1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,9 +8,18 @@ public class Main {
         people.add(new Person("Пеппи", "Большой длинный носок", 110));
         people.add(new Person("Глория", "Сильно старые джинсы", 102));
         people.add(new Person("Александ", "Повелитель урожая плодородия всея савхоза колхоза и заноза", 55));
-        people.add(new Person("Володимир", "Князь всея Руси и ближайших земель наших и не совсем", 52));
+        people.add(new Person("Володимир", "Князь всея Руси и ближайших земель наших и не совсем", 58));
 
-        Collections.sort(people, new PersonComparator(5));
+        people.sort((Person o1, Person o2) -> {
+            int maxCool = 5;
+            int oneCoolRating = o1.getSurname().split(" ").length;
+            int twoCoolRating = o2.getSurname().split(" ").length;
+            if (oneCoolRating == twoCoolRating || (twoCoolRating >= maxCool & twoCoolRating >= maxCool)) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            } else
+                return Integer.compare(oneCoolRating, twoCoolRating);
+        });
+
         System.out.println(people);
     }
 }
